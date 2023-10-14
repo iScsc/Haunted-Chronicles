ab6be25d1319779c30073572404f5f8f7238fef1
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2023-10-15 00:23:53 +0200

    Adapted test for username displays

--- interpratorTest.py
+++ interpratorTest.py
@@ -2,7 +2,7 @@ from player import Player
 
 def main():
     
-    msg = "[(a,(1,1,1),(1,1),(1,1)),(b,(1,2,3),(1,2),(3,4)),(c,(11,12,12),(25,31),(87,32))]"
+    msg = "[('a',(1,1,1),(1,1),(1,1)),(b,(1,2,3),(1,2),(3,4)),(c'd',(11,12,12),(25,31),(87,32))]"
     players = Player.toPlayers(msg)
     
     for player in players:
