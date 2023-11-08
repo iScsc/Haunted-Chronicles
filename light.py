36e57c0227c15369e150ff87ae15778882493f5b
Tudual <tudual.durand@gmail.com>
2023-11-08 20:37:11 +0100

    add object light

---
+++ light.py
@@ -0,0 +1,18 @@
+
+class Light:
+    
+    BASE_COLOR = (255, 255, 255)
+    BASE_POSITION = (10, 10)
+    BASE_SIZE = (0, 0)
+
+    def __init__(self,position) -> None:
+        self.color = Light.BASE_COLOR
+        self.position = position
+        self.size = Light.BASE_SIZE
+
+
+
+
+
+
+
