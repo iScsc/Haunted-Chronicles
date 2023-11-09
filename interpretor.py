b79a6ed4eaedad5cae29decbce123248b0f80d6e
BlueNZ <zadigballesio@gmail.com>
2023-11-09 19:37:58 +0100

    toWalls now uses the interpretor

--- interpretor.py
+++ interpretor.py
@@ -45,28 +45,40 @@ def spc(string):
 
 def interp(playerString, **kwargs):
     from player import Player
+    from wall import Wall
     values=spc(playerString)
     assert(len(values)==len(kwargs))
     i=0
     for arg in kwargs:
+        
         if type(kwargs[arg])==str:
             kwargs[arg]=values[i]
+            
         elif type(kwargs[arg])==int:
             kwargs[arg]=int(values[i])
+            
         elif type(kwargs[arg])==float:
             kwargs[arg]=float(values[i])
+            
         elif type(kwargs[arg])==common.Color:
             d=interp(values[i],r=0,g=0,b=0)
             kwargs[arg]=common.Color(d['r'],d['g'],d['b'])
+            
         elif type(kwargs[arg])==common.Position:
             d=interp(values[i],x=0,y=0)
             kwargs[arg]=common.Position(d['x'],d['y'])
+            
         elif type(kwargs[arg])==common.Size:
             d=interp(values[i],w=0,h=0)
             kwargs[arg]=common.Size(d['w'],d['h'])
+            
         elif type(kwargs[arg])==Player:
             d=interp(values[i],username="",color=common.Color(),position=common.Position(),size=common.Size())
             kwargs[arg]=Player("",d['username'],d['color'],d['position'],d['size'])
+            
+        elif type(kwargs[arg])==Wall:
+            d=interp(values[i],id=0,color=common.Color(),position=common.Position(),size=common.Size())
+            kwargs[arg]=Wall(d['id'],d['color'],d['position'],d['size'])
         
         i+=1
     return kwargs
