0e2fd1352023c1b104a29525ebe407c07b0769a3
BlueNZ <zadigballesio@gmail.com>
2023-11-24 15:36:11 +0100

    Added docstring and comments

--- interpretor.py
+++ interpretor.py
@@ -2,41 +2,57 @@ import common
 
 
 
-def spc(string):
+def spc(string:str):
+    """Splits a given string on parenthesis and commas, taking just into acount first level parenthesis.
+    For instance: "(abc,def),ghi,(jkl,(mno))" -> ["abc,def","ghi","jkl,(mno)"]
+
+    Args:
+        string (str): The string to split
+
+    Returns:
+        list(str): The list of substrings 
+    """
     
     #getting parenthesis index
-    po=[]
-    pf=[]
-    indexo=0
-    indexf=0
-    while indexo!=-1 and indexf!=-1:
-        indexo=string.find('(',indexo)
+    
+    po=[] #list of opening parenthesis
+    pf=[] #list of closing parenthesis
+    indexo=0 #index of the last opening parenthesis found
+    indexf=0 #index of the last closing parenthesis found
+    
+    while indexo!=-1 and indexf!=-1: # While an opening and closing parenthesis have been found
+        indexo=string.find('(',indexo) # find the next  parethesis index starting from the last one
         indexf=string.find(')',indexf)
-        if (indexo!=-1 and indexf!=-1):
-            if (len(pf)>0 and indexo<pf[-1]):
-                pf[-1]=indexf
+        if (indexo!=-1 and indexf!=-1): # If an opening and closing parenthesis have been found
+            if (len(pf)>0 and indexo<pf[-1]): # If the situation is "...(...(...)...)..." the new opening parenthesis is a lower level one
+                pf[-1]=indexf # last closing parenthesis was a lower level one (inside other parenthesis) and should not be taken into account
                 indexf+=1
                 indexo+=1
-            else :
+            else : # first level parenthesis
                 po.append(indexo)
                 indexo+=1
                 pf.append(indexf)
                 indexf+=1
     
     #getting substrings
-    substr=[]
-    if (len(po)==0):
+    
+    substr=[] # list of substrings to be returned
+    
+    if (len(po)==0): # no parenthesis found: split the string only on commas
         substr+=string.split(',')
-    elif (po[0]!=0):
+        
+    elif (po[0]!=0): # take the begining of the string (before the first parenthesis) and split it on commas 
         substr+=(string[0:po[0]]).split(',')
-    for i in range(len(po)):
+        
+    for i in range(len(po)): # for each pair of '(' ')', gets what's inbetween
         substr.append(string[po[i]+1:pf[i]])
-        if (i<len(po)-1):
+        if (i<len(po)-1): # gets what's inbetween pairs and split it on commas
             substr+=(string[pf[i]+1:po[i+1]]).split(',')
-    if (len(pf)>0 and pf[-1]!=len(string)-1):
+            
+    if (len(pf)>0 and pf[-1]!=len(string)-1): # take the end of the string (after the first parenthesis) and split it on commas
         substr+=(string[pf[-1]+1:]).split(',')
         
-    #epurating from void substring and residual apostrophe
+    #epurating from void substring, residual apostrophe and unwanted substrings
     substr=[s for s in substr if s not in ['',"'","['","']",'[',']']]
     
     return substr
@@ -44,17 +60,39 @@ def spc(string):
 
 
 def interp(string, **kwargs):
+    """Interprets a given string according to the specification in **kwargs
     
+    Supported types are:
+        int, float, str, Color, Position, Size, Player, Wall and list
+
+    Args:
+        string (str): The string to interpret.
+        **kwargs (dict[str|Any]): kwargs should match the type of the desired variable.
+            Ex: for an int i of value a one shoulde use "interp('a',i=0)"
+    
+    Special cases:
+    
+        list: interp("[...]", list=[a,i]) where a matches the type of the list elements and i is the number of elements to look for.
+        
+        tuples: 
+
+    Returns:
+        dict[str|Any]: updated kwargs
+    """
+    
+    # local imports to avoid circular imports
     from player import Player
     from wall import Wall
     
+    # splits the given string
     values=spc(string)
-    # assert(len(values)==len(kwargs))
     
+    # current index
     i=0
     
     for arg in kwargs:
         
+        # basic types 
         if type(kwargs[arg])==str:
             kwargs[arg]=values[i]
             
@@ -63,7 +101,8 @@ def interp(string, **kwargs):
             
         elif type(kwargs[arg])==float:
             kwargs[arg]=float(values[i])
-            
+        
+        # common types, uses recursivity
         elif type(kwargs[arg])==common.Color:
             d=interp(values[i],r=0,g=0,b=0)
             kwargs[arg]=common.Color(d['r'],d['g'],d['b'])
@@ -75,7 +114,8 @@ def interp(string, **kwargs):
         elif type(kwargs[arg])==common.Size:
             d=interp(values[i],w=0,h=0)
             kwargs[arg]=common.Size(d['w'],d['h'])
-            
+        
+        # specific types, uses recursivity
         elif type(kwargs[arg])==Player:
             d=interp(values[i],username="",color=common.Color(),position=common.Position(),size=common.Size())
             kwargs[arg]=Player("",d['username'],d['color'],d['position'],d['size'])
@@ -84,12 +124,15 @@ def interp(string, **kwargs):
             d=interp(values[i],id=0,color=common.Color(),position=common.Position(),size=common.Size())
             kwargs[arg]=Wall(d['id'],d['color'],d['position'],d['size'])
         
+        # list, uses recursivity, usually len(kwargs)!=len(values)
         elif type(kwargs[arg])==list:
-            for j in range(kwargs[arg][1]):
+            # kwargs[arg]=[a,k] with a matching the type of the desired elements, k the number of such elements to fetch
+            for j in range(kwargs[arg][1]): # in range(k)
                 d=interp('('+values[i+j]+')',element=kwargs[arg][0])
                 kwargs[arg].append(d['element'])
-            del kwargs[arg][0]
-            del kwargs[arg][0]
+            i+=kwargs[arg][1] # shift current index
+            del kwargs[arg][0] # removes a from the list
+            del kwargs[arg][0] # removes k from the list
         
         i+=1
         
