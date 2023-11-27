3c1dfa9c412f80cef61c0a41de04046ff625f972
BlueNZ <zadigballesio@gmail.com>
2023-11-27 10:30:11 +0100

    resolving conflict

--- interpretor.py
+++ interpretor.py
@@ -78,7 +78,10 @@ def interp(string, **kwargs):
             
         elif type(kwargs[arg])==Player:
             d=interp(values[i],username="",color=common.Color(),position=common.Position(),size=common.Size())
-            kwargs[arg]=Player(d['username'],d['color'],d['position'],d['size'])
+            # get rid of '' around usernames due to str(str)
+            if len(d['username'])>2 and ((d['username'][0]=="'" and d['username'][-1]=="'") or (d['username'][0]=="\"" and d['username'][-1]=="\"")):
+                d['username']=d['username'][1:-1]
+            kwargs[arg]=Player("",d['username'],d['color'],d['position'],d['size'])
             
         elif type(kwargs[arg])==Wall:
             d=interp(values[i],id=0,color=common.Color(),position=common.Position(),size=common.Size())
