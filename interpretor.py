379295df26764539d727bb725d283a12e3681dc9
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2023-12-04 10:03:02 +0100

    Avoid printing

--- interpretor.py
+++ interpretor.py
@@ -20,7 +20,6 @@ def spc(string:str, strip=False):
     indexo=0 #index of the last opening parenthesis found
     indexf=0 #index of the last closing parenthesis found
     
-    print("string =", string)
     forbidden_char = ["[", "]", "'", '"']
     
     if strip:
